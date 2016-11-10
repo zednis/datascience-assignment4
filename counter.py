636c380931be3b8cf302258f71dbeea9c8705a25
Corey Byrne <byrnec3@rpi.edu>
2016-11-09 23:16:26 -0500

    init commit

---
+++ counter.py
@@ -0,0 +1,10 @@
+import pandas
+import argparse
+
+parser = argparse.ArgumentParser(description='Value count for columns.')
+parser.add_argument('file', metavar='F', help='source file')
+parser.add_argument('col', metavar='C', help='column to count values')
+args = parser.parse_args()
+
+data = pandas.read_csv(args.file)
+print data[args.col].value_counts()
